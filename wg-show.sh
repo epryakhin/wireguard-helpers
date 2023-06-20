bb9724ed0af60c56da6b64884ae8cbbd27bc6931
Evgeny Pryakhin <breath1988@gmail.com>
2023-06-20 18:49:47 +0300

    wg show helper added, shows peers user-device

---
+++ wg-show.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Require root to see wg-related settings
+if ! [ "$(id -u)" = "0" ]; then
+    echo "ERROR: sudo is required to configure WireGuard clients"
+    exit 1
+fi
+
+# Help and basic error checking
+if [ $# -ge 1 -a "$1" == "--help" ]; then
+        echo "Usage:"
+        echo "$(basename "$0") <interface:[wg0]>"
+        exit 1
+fi
+
+interface="${1:-wg0}"
+conf_file="/etc/wireguard/$interface.conf"
+
+if [ ! -f conf_file ]; then
+ echo "Interface $interface not configured"
+ exit 1
+fi
+
+conf=$(< $conf_file)
+
+wg show "$interface" | while read -r line
+do
+ res=$(echo "$line" | grep peer)
+ if [ -z "$res" ]; then
+  echo $line
+ else
+  key=$(echo $line | sed 's/\(peer: \)\(.*\)/\2/')
+  name=$(echo "$conf" | grep -B1 "$key" | grep -v "$key" | sed 's/\# //')
+  echo "peer: ${key} | $name"
+ fi
+done
