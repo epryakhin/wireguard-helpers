a9b311cdebfb77b293654d190cf042cfe7dfa638
epryakhin <breath1988@gmail.com>
2023-07-03 18:50:27 +0200

    setup ipv4/ipv6 forwarding

--- 01-server-prepare.sh
+++ 01-server-prepare.sh
@@ -49,3 +49,8 @@ ufw allow "$port"
 ufw show added
 # enable
 ufw enable
+
+# enable ipv4/ipv6 forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.default.forwarding=1
+sysctl -w net.ipv6.conf.all.forwarding=1
