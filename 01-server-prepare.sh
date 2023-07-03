f5ceb4df2ee218d16b292825ab0d77afa56eda0c
epryakhin <breath1988@gmail.com>
2023-07-03 11:36:03 +0200

    added server prepare script

---
+++ 01-server-prepare.sh
@@ -0,0 +1,44 @@
+# update server first
+apt update
+apt upgrade
+
+# --- create new user ---
+# read user name and password from user input
+echo -n "Enter user name: "
+read $user
+adduser "$user"
+# read user from variable
+usermod -aG sudo "$user"
+
+# --- configure SSH ---
+# generate random port number
+port=$(( $RANDOM + 10000 ))
+echo "Generated random SSH port number: $port"
+# set sshd port
+sed "s/#Port 22/Port $port/" /etc/ssh/sshd_config
+# allow only public key auth, no root login
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+# copy public key
+mkdir "/home/$user/.ssh/"
+cp key "/home/$user/.ssh/authorized_keys"
+chown -R "$user":"$user" "/home/$user/.ssh"
+# restart sshd.service
+read -p "SSH service will be restarted on port [$port] with no password login and no root login. Are use sure? [Yy]: " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+ systemctl reload sshd.service
+fi
+
+# configure ufw
+# set defaults
+ufw default deny incoming
+ufw default allow outgoing
+# allow new ssh port
+ufw allow "$port"
+# print rules
+ufw show added
+# enable
+ufw enable
